5903a931239dc842f186ca9d155ebdff7d56a8a5
ArgentinaG29 <margent.garz1926@gmail.com>
2022-11-05 16:32:38 -0600

    Finalizado

--- Hackathon/Ejercicios.cs
+++ Hackathon/Ejercicios.cs
@@ -10,8 +10,207 @@ namespace Hackathon
     {
         public bool Scramble(string cadena1, string cadena2)
         {
+            List<string> caracteres1 = new List<string>();
+            List<string> caracteres2 = new List<string>();
 
-            return true;
+            for (int i = 0; i < cadena1.Length; i++)
+            {
+                caracteres1.Add(cadena1.Substring(i, 1).ToLower());
+                caracteres2.Add(cadena2.Substring(i, 1).ToLower());
+            }
+            
+            for (int i = 0; i < caracteres2.Count; i++)
+            {                                
+                for (int j = 0; j < caracteres1.Count; j++)
+                {
+                    if(caracteres2[i] == caracteres1[j])
+                    {
+                        caracteres1.RemoveAt(j);                        
+                        break;
+                    }
+                }
+            }
+
+            if(caracteres1.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+
+        public string Justificador(string texto, int num)
+        {
+            //VER PASO A PASO
+            string mostrar = "";
+
+            string buscador = "";
+            int contador = 0;
+            int cadena = 0;
+            mostrar = "\"";
+            bool terminado = false;
+            string letra = "";
+            //contar cuantas letras tiene la palabra
+            while (terminado == false)
+            {
+                if (texto == "")
+                {
+                    terminado = true;
+                    letra = "";
+                }
+                else
+                {
+                    letra = texto.Substring(0, 1);
+                    texto = texto.Remove(0, 1);
+                }
+                if (letra == " ")
+                {
+                    string prueba = mostrar + buscador;
+                    if (prueba.Length <= num)
+                    {
+                        if (prueba.Length == 8)
+                                {
+                            mostrar += " " + buscador + "\"";
+                            buscador = "";
+                            Console.WriteLine(mostrar);
+                            mostrar = "\"";
+                        }
+                        else
+                        {
+                            mostrar += buscador + " ";
+                            buscador = "";
+                        }
+
+                    }
+                    else
+                    {
+                        //devolver la palabra
+                        texto = buscador + " " + texto;
+                        //BUSCAR CUADRAR LA CADENA
+                        int faltante = num - mostrar.Length;
+                        string[] oracion = mostrar.Split(' ');
+                        int cant = oracion.Length;
+                        int veri = 0;
+                        int repartido = 0;
+                        if(cant != 0)
+                        {
+                            repartido = faltante / (cant);
+                            veri = faltante - (repartido * cant) - 1;
+                        }
+                        
+                        string espacios = "";
+                        if (veri != 0)
+                        {
+                            for (int i = 0; i < veri; i++)
+                            {
+                                espacios += " ";
+                            };
+                            mostrar = oracion[0] + " " + espacios;
+                            for (int i = 1; i < cant; i++)
+                            {
+                                for (int j = 0; j < repartido; j++)
+                                {
+                                    espacios += " ";
+                                };
+                                mostrar += oracion[i] + " " + espacios;
+                            }
+                            Console.WriteLine(mostrar + "\"");
+                            mostrar = "\"";
+                            buscador = "";
+                        }
+                        else
+                        {
+                            for (int i = 0; i < cant; i++)
+                            {
+                                for (int j = 0; j < repartido; j++)
+                                {
+                                    espacios += " ";
+                                };
+                                if (i == cant - 1)
+                                {
+                                    mostrar += oracion[i] + espacios;
+                                }
+                                else
+                                {
+                                    mostrar += oracion[i] + " " + espacios;
+                                }
+                            }
+                            Console.WriteLine(mostrar + "\"");
+                            mostrar = "\"";
+                            buscador = "";
+                        }
+
+                    }
+                }
+                else
+                {
+                    buscador += letra;
+                }
+            }
+            string espaciosA = "";
+            texto = buscador + texto;
+            for (int j = 0; j < (num - texto.Length); j++)
+            {
+                espaciosA += " ";
+            };
+
+            mostrar = "\"" + texto + espaciosA;
+            return mostrar + "\"";            
+        }
+
+        public int AbuelaBinaria(int total, int n1, int n2)
+        {
+            string binario1, binario2;
+            int cantidad = 0;
+            binario1 = Binario(n1);
+            binario2 = Binario(n2);
+
+            for (int i = 0; i < binario1.Length; i++)
+            {
+                if(binario1.Substring(i,1) == "1")
+                {
+                    cantidad++;
+                }                
+            }
+
+            for (int i = 0; i < binario2.Length; i++)
+            {
+                if (binario2.Substring(i, 1) == "1")
+                {
+                    cantidad++;
+                }
+            }
+
+            return cantidad;
+
+        }
+
+        string Binario(int n)
+        {
+            string binario = "";
+            if(n != 0)
+            {
+                while (n > 0)
+                {
+                    if (n % 2 == 0)
+                    {
+                        binario = "0" + binario;
+                    }
+                    else
+                    {
+                        binario = "1" + binario;
+                    }
+                    n = n / 2;
+                }
+                return binario;
+            }
+            else
+            {
+                return "0";
+            }
+            
         }
 
     }
--- Hackathon/Program.cs
+++ Hackathon/Program.cs
@@ -11,7 +11,104 @@ namespace Hackathon
         static void Main(string[] args)
         {
             Ejercicios e = new Ejercicios();
-            
+            Console.WriteLine("Seleccione el ejercicio a ejecutar: ");
+            Console.WriteLine("1. Scramble");
+            Console.WriteLine("2. Justificador de texto");
+            Console.WriteLine("3. Cadenas subsecuentes");
+            Console.WriteLine("4. Abuela binaria");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    //Scramble
+                    Console.Clear();
+                    Console.WriteLine("Ingrese cadena 1: ");
+                    string cadena1 = Console.ReadLine();
+                    Console.WriteLine("Ingrese cadena 2: ");
+                    string cadena2 = Console.ReadLine();
+
+                    if (e.Scramble(cadena1, cadena2))
+                    {
+                        Console.WriteLine("Resultado: TRUE");                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Resultado: FALSE");                       
+                    }                    
+                    break;
+                case 2:
+                    //Justificador de texto
+                    Console.Clear();
+                    Console.WriteLine("Ingrese la cantidad maxima de caracteres por linea");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese el texto a justificar");
+                    string texto = Console.ReadLine();
+                    
+                    Console.WriteLine(e.Justificador(texto, num));
+                    break;
+                case 3:
+                    Console.WriteLine("Candena 1");
+                    string cad1 = Console.ReadLine().ToLower();
+                    Console.WriteLine("Candena 2");
+                    string cad2 = Console.ReadLine();
+                    string cad3 = cad2;
+                    string iguales = "";
+                    bool terminadoo = false;
+                    while (terminadoo == false)
+                    {
+                        string com = cad1.Substring(0, 1).ToLower();
+                        cad1 = cad1.Remove(0, 1);
+                        for (int i = 0; i < cad3.Length; i++)
+                        {
+                            string comprador = cad2.Substring(0, 1);
+                            cad2 = cad2.Remove(0, 1);
+                            if (comprador == com)
+                            {
+                                iguales += com;
+                            }
+                        }
+                        cad2 = cad3;
+                        if (cad1 == "")
+                        {
+                            terminadoo = true;
+                        }
+                    }
+                    // VERIFICAR QUE NO SEAN IGUALES
+                    string ig = iguales.Trim();
+                    Console.WriteLine(ig);
+
+
+                    break;
+                case 4:
+                    //Abuela binaria
+                    Console.Clear();
+                    Console.WriteLine("Ingrese primer numero (N): ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese segundo numero (n1): ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese tercer numero (n2): ");
+                    int num3 = Convert.ToInt32(Console.ReadLine());
+                    if((num2+num3)==num1)
+                    {
+                        int resultado = e.AbuelaBinaria(num1, num2, num3);
+                        Console.WriteLine("La abuela le dará a su nieta "+ resultado.ToString()+" avellana(s)");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número N debe ser resultado de la suma de n1 y n2");
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Ingrese una opcion correcta");
+                    break;
+            }
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+
         }
     }
 }
