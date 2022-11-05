10cebab0b09976b8f97ef532a272f356d225326d
ArgentinaG29 <margent.garz1926@gmail.com>
2022-11-05 14:29:43 -0600

    Agregar archivos de proyecto.

---
+++ Hackathon/Ejercicios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    public class Ejercicios
+    {
+        public bool Scramble(string cadena1, string cadena2)
+        {
+
+            return true;
+        }
+
+    }
+}
